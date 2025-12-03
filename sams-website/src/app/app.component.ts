d42f643dd8103d8aeb3364304660290a77144179
Andrew Rohde <aprohde23@gmail.com>
2025-12-03 15:31:11 -0500

    updated look of site

--- sams-website/src/app/app.component.ts
+++ sams-website/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   imports: [
     RouterOutlet,
     NavBarComponent,
+    HeaderComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
