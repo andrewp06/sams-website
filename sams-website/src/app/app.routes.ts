01e79eaf484ac8ecf6ef4d4596c788fcd35d100e
Andrew Rohde <aprohde23@gmail.com>
2025-12-03 12:47:44 -0500

    added router linker between components

--- sams-website/src/app/app.routes.ts
+++ sams-website/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PhotosComponent } from './photos/photos.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "photos", component: PhotosComponent},
+    {path: "contact", component: ContactPageComponent},
+    {path: "about", component: AboutPageComponent}
+];
